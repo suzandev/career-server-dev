e56555ba5be6f6b98da365ed135e17522cc6a11d
Suzan Chandra <suzanchandra@Suzans-Mac-mini.local>
2026-02-25 23:41:52 +0600

    create a collection for collect data in mongodb database

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
+// ====== MongoDB URI ======
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.on5po.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,6 +26,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const jobsCollection = client.db("career-server-dev").collection("jobs");
+
+    // job API
+    app.get("/jobs", async (req, res) => {
+      const cursor = jobsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
